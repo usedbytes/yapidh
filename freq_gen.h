9cbf2d7d3999e4dd02c34c23b8c6dc70d1298efd
Brian Starkey <stark3y@gmail.com>
2019-03-20 20:26:59 +0000

    freq_gen: Add play/pause/reset

--- freq_gen.h
+++ freq_gen.h
@@ -3,4 +3,8 @@
 
 void freq_source_add_note(struct source *s, uint32_t timestamp, int note, uint32_t duration);
 struct source *freq_source_create(int step, int dir, int pwdn, int direction);
+void freq_source_play_pause(struct source *s, bool play);
+void freq_source_set_timestamp(struct source *s, uint32_t timestamp);
+void freq_source_clear(struct source *s);
+uint32_t freq_source_timestamp(struct source *s);
 uint32_t us_to_samples(uint32_t us);
